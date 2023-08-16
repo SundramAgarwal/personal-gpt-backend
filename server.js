a5abd40d0a90d3a2536569392e81f1fcc1cd9061
Sundram Agarwal <agarwal.sundaram@gmail.com>
2023-08-16 15:12:47 +0530

    commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const errorHandler = require("./middlewares/errorMiddleware");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://personal-gpt-one.vercel.app/"],
+    origin: ["http://localhost:3000", "https://personal-gpt-one.vercel.app"],
     credentials: true,
   })
 );
